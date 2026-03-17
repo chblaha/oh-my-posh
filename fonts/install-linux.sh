0155a35e8abf811241e7eb339e1f35cb8b006881
Christian Blaha <100966933+chblaha@users.noreply.github.com>
2026-03-17 10:21:34 +0100

    CR

--- fonts/install-linux.sh
+++ fonts/install-linux.sh
@@ -4,9 +4,9 @@ set -e
 FONT_DIR="$HOME/.local/share/fonts"
 mkdir -p "$FONT_DIR"
 
-FONT="CaskaydiaCove Nerd Font"
+FONT="CascadiaCode Nerd Font"
 
-if fc-list | grep -qi "Caskaydia"; then
+if fc-list | grep -qi "Cascadia"; then
   echo "Font bereits installiert"
   exit 0
 fi
@@ -14,8 +14,8 @@ fi
 TMP=$(mktemp -d)
 cd "$TMP"
 
-curl -LO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CaskaydiaCove.zip
-unzip -q CaskaydiaCove.zip
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip
+unzip -q CascadiaCode.zip
 cp *.ttf "$FONT_DIR"
 
 fc-cache -fv

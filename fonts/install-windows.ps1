88f29cb3c33851456bd1e3be7bb05b6b9c09504d
Christian Blaha <100966933+chblaha@users.noreply.github.com>
2026-03-17 10:19:21 +0100

    CR

--- fonts/install-windows.ps1
+++ fonts/install-windows.ps1
@@ -1,4 +1,6 @@
-#$FontName = "CaskaydiaCove Nerd Font"
+$FontDir = "$env:LOCALAPPDATA\Microsoft\Windows\Fonts"
+$RegPath = "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Fonts"
+
 
 if (Get-ChildItem "$env:WINDIR\Fonts" | Where-Object { $_.Name -like "*Caskaydia*" }) {
     Write-Host "Font bereits installiert"
@@ -6,12 +8,24 @@ if (Get-ChildItem "$env:WINDIR\Fonts" | Where-Object { $_.Name -like "*Caskaydia
 }
 
 $Temp = New-Item -ItemType Directory -Force -Path "$env:TEMP\nerd-font"
-Invoke-WebRequest `
-    -Uri "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CaskaydiaCove.zip" `
-    -OutFile "$Temp\font.zip"
+Invoke-WebRequest -Uri "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip" -OutFile "$Temp\font.zip"
 
-Expand-Archive "$Temp\font.zip" "$Temp\font"
 
-Get-ChildItem "$Temp\font" -Filter *.ttf | ForEach-Object {
-    Copy-Item $_.FullName "$env:WINDIR\Fonts"
+Expand-Archive "$Temp\font.zip" "$Temp\font" -Force
+
+Get-ChildItem "$Temp\font" -Filter "*.ttf" | ForEach-Object {
+    $Target = Join-Path $FontDir $_.Name
+
+    if (-not (Test-Path $Target)) {
+        Copy-Item $_.FullName $Target
+
+        New-ItemProperty `
+            -Path $RegPath `
+            -Name $_.Name `
+            -PropertyType String `
+            -Value $_.Name `
+            -Force | Out-Null
+    }
 }
+
+Write-Host "✅ Nerd Font (per user) installiert"
